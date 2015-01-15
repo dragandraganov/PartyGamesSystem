d165e886644399a87d72fa3c70969e249a371e36
Dr4g0 <d_d0003@yahoo.com>
2015-01-15 10:30:26 +0200

    Migrations integrated plus AuditInfoRules and DeletableEntityRules

--- Source/Data/PartyGamesSystem.Data/PartyGamesSystemDbContext.cs
+++ Source/Data/PartyGamesSystem.Data/PartyGamesSystemDbContext.cs
@@ -2,6 +2,9 @@
 using PartyGamesSystem.Data.Models;
 using System;
 using System.Linq;
+using System.Data.Entity;
+using PartyGamesSystem.Data.Migrations;
+using PartyGamesSystem.Data.Contracts.Models;
 
 namespace PartyGamesSystem.Data
 {
@@ -10,11 +13,61 @@ namespace PartyGamesSystem.Data
         public PartyGamesSystemDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<PartyGamesSystemDbContext, Configuration>());
         }
 
         public static PartyGamesSystemDbContext Create()
         {
             return new PartyGamesSystemDbContext();
+        }
+
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            this.ApplyDeletableEntityRules();
+            return base.SaveChanges();
+        }
+
+
+        private void ApplyAuditInfoRules()
+        {
+            // Approach via @julielerman: http://bit.ly/123661P
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditInfo && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditInfo)entry.Entity;
+
+                if (entry.State == EntityState.Added)
+                {
+                    if (!entity.PreserveCreatedOn)
+                    {
+                        entity.CreatedOn = DateTime.Now;
+                    }
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
+        }
+
+        private void ApplyDeletableEntityRules()
+        {
+            // Approach via @julielerman: http://bit.ly/123661P
+            foreach (
+                var entry in
+                    this.ChangeTracker.Entries()
+                        .Where(e => e.Entity is IDeletableEntity && (e.State == EntityState.Deleted)))
+            {
+                var entity = (IDeletableEntity)entry.Entity;
+
+                entity.DeletedOn = DateTime.Now;
+                entity.IsDeleted = true;
+                entry.State = EntityState.Modified;
+            }
         }
     }
 }
