ebbbb135e65ce3f041fd308e71260267d9bcce6f
Dr4g0 <d_d0003@yahoo.com>
2015-01-15 10:13:43 +0200

    Added Repository pattern with Audit Models

---
+++ Source/Data/PartyGamesSystem.Data.Contracts/Models/IDeletableEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace PartyGamesSystem.Data.Contracts.Models
+{
+    using System;
+
+    public interface IDeletableEntity
+    {
+        bool IsDeleted { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+    }
+}
