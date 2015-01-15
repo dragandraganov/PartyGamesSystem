10a4cff859b9a5315e15c5fca837992696bf884f
Dr4g0 <d_d0003@yahoo.com>
2015-01-15 10:35:59 +0200

    Added IAuditInfo and IDeletableEntity rules to User model

--- Source/Data/PartyGamesSystem.Data.Models/User.cs
+++ Source/Data/PartyGamesSystem.Data.Models/User.cs
@@ -1,16 +1,25 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using PartyGamesSystem.Data.Contracts.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace PartyGamesSystem.Data.Models
 {
-    public class User : IdentityUser
+    public class User : IdentityUser, IAuditInfo, IDeletableEntity
     {
+        public DateTime CreatedOn { get; set; }
+
+        public bool PreserveCreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
--- Source/Data/PartyGamesSystem.Data/Migrations/Configuration.cs
+++ Source/Data/PartyGamesSystem.Data/Migrations/Configuration.cs
@@ -9,23 +9,13 @@ namespace PartyGamesSystem.Data.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            this.AutomaticMigrationsEnabled = true;
+            // TODO: Remove in production
+            this.AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(PartyGamesSystem.Data.PartyGamesSystemDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
         }
     }
 }
