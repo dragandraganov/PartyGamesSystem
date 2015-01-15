fa192a07be42301db09f7ec2c53da0980182f6e8
Dr4g0 <d_d0003@yahoo.com>
2015-01-15 10:23:01 +0200

    Moved DbContext and User model in optimal destinations

---
+++ Source/Data/PartyGamesSystem.Data/PartyGamesSystemDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using PartyGamesSystem.Data.Models;
+using System;
+using System.Linq;
+
+namespace PartyGamesSystem.Data
+{
+    public class PartyGamesSystemDbContext : IdentityDbContext<User>
+    {
+        public PartyGamesSystemDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static PartyGamesSystemDbContext Create()
+        {
+            return new PartyGamesSystemDbContext();
+        }
+    }
+}
--- Source/Web/PartyGamesSystem.Web/App_Start/Startup.Auth.cs
+++ Source/Web/PartyGamesSystem.Web/App_Start/Startup.Auth.cs
@@ -6,6 +6,8 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using PartyGamesSystem.Web.Models;
+using PartyGamesSystem.Data.Models;
+using PartyGamesSystem.Data;
 
 namespace PartyGamesSystem.Web
 {
@@ -15,7 +17,7 @@ namespace PartyGamesSystem.Web
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext(PartyGamesSystemDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
@@ -30,7 +32,7 @@ namespace PartyGamesSystem.Web
                 {
                     // Enables the application to validate the security stamp when the user logs in.
                     // This is a security feature which is used when you change a password or add an external login to your account.  
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, User>(
                         validateInterval: TimeSpan.FromMinutes(30),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
