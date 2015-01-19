90fb60804fd6383377aa25f03b389286f1c526b3
Dr4g0 <d_d0003@yahoo.com>
2015-01-19 15:55:23 +0200

    Administration was deleted

--- Source/Data/PartyGamesSystem.Data/PartyGamesSystemDbContext.cs
+++ Source/Data/PartyGamesSystem.Data/PartyGamesSystemDbContext.cs
@@ -30,7 +30,6 @@ namespace PartyGamesSystem.Data
         public override int SaveChanges()
         {
             this.ApplyAuditInfoRules();
-            this.ApplyDeletableEntityRules();
             return base.SaveChanges();
         }
 
@@ -63,22 +62,6 @@ namespace PartyGamesSystem.Data
                 {
                     entity.ModifiedOn = DateTime.Now;
                 }
-            }
-        }
-
-        private void ApplyDeletableEntityRules()
-        {
-            // Approach via @julielerman: http://bit.ly/123661P
-            foreach (
-                var entry in
-                    this.ChangeTracker.Entries()
-                        .Where(e => e.Entity is IDeletableEntity && (e.State == EntityState.Deleted)))
-            {
-                var entity = (IDeletableEntity)entry.Entity;
-
-                entity.DeletedOn = DateTime.Now;
-                entity.IsDeleted = true;
-                entry.State = EntityState.Modified;
             }
         }
     }
