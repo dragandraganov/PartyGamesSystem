7e2d6fd21a3f64e18903d12bbc07b6e7902a1cb2
Dr4g0 <d_d0003@yahoo.com>
2015-01-22 12:06:03 +0200

    Added Details view for ManagePartyGamesController

--- Source/Web/PartyGamesSystem.Web/Controllers/ManagePartyGamesController.cs
+++ Source/Web/PartyGamesSystem.Web/Controllers/ManagePartyGamesController.cs
@@ -8,6 +8,7 @@ using AutoMapper;
 using PartyGamesSystem.Data.Models;
 using System.IO;
 using System.Web;
+using PartyGamesSystem.Common;
 
 namespace PartyGamesSystem.Web.Controllers
 {
@@ -28,6 +29,23 @@ namespace PartyGamesSystem.Web.Controllers
                 .Project()
                 .To<PartyGameViewModel>();
             return View(allPartyGames);
+        }
+
+        //GET: Edit party game
+        public ActionResult Details(int id)
+        {
+            var existingPartyGame = this.Data
+                .PartyGames
+                .AllWithDeleted()
+                .Where(pg => pg.Id == id)
+                .Project()
+                .To<PartyGameViewModel>()
+                .FirstOrDefault();
+            if (existingPartyGame == null)
+            {
+                throw new HttpException(404, "Party game not found");
+            }
+            return View(existingPartyGame);
         }
 
         //GET: Create new game
@@ -65,11 +83,100 @@ namespace PartyGamesSystem.Web.Controllers
                 this.Data.PartyGames.Add(newPartyGame);
                 this.Data.SaveChanges();
 
-                return RedirectToAction("Index", "FunnyPartyGames");
+                return RedirectToAction("Index", "ManagePartyGames");
             }
 
             partyGame.Categories = this.GetCategories();
             return View(partyGame);
         }
+
+        //GET: Edit party game
+        public ActionResult Edit(int id)
+        {
+            var existingPartyGame = this.Data
+                .PartyGames
+                .AllWithDeleted()
+                .Where(pg => pg.Id == id)
+                .Project()
+                .To<PartyGameViewModel>()
+                .FirstOrDefault();
+            if (existingPartyGame == null)
+            {
+                throw new HttpException(404, "Party game not found");
+            }
+            var allCategories = this.GetCategories();
+            existingPartyGame.Categories = new SelectList(allCategories, "Value", "Text");
+            return View(existingPartyGame);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(PartyGameViewModel partyGame)
+        {
+            if (partyGame != null && ModelState.IsValid)
+            {
+                var existingPartyGame = this.Data
+                    .PartyGames
+                    .GetById(partyGame.Id);
+                Mapper.Map(partyGame, existingPartyGame);
+                if (partyGame.UploadedImage != null)
+                {
+                    using (var memory = new MemoryStream())
+                    {
+                        partyGame.UploadedImage.InputStream.CopyTo(memory);
+                        var content = memory.GetBuffer();
+
+                        existingPartyGame.Image = new Image
+                        {
+                            Content = content,
+                            FileExtension = partyGame.UploadedImage.FileName.Split(new[] { '.' }).Last()
+                        };
+                    }
+                }
+                this.Data.PartyGames.Update(existingPartyGame);
+                this.Data.SaveChanges();
+
+                return RedirectToAction("Index", "ManagePartyGames");
+            }
+
+            partyGame.Categories = this.GetCategories();
+
+            return View(partyGame);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var existingPartyGame = this.Data
+                .PartyGames
+                .AllWithDeleted()
+                .Where(pg => pg.Id == id)
+                .Project()
+                .To<PartyGameViewModel>()
+                .FirstOrDefault();
+            if (existingPartyGame == null)
+            {
+                throw new HttpException(404, "Party game not found");
+            }
+            return View(existingPartyGame);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(PartyGameViewModel partyGame)
+        {
+            if (partyGame != null && ModelState.IsValid)
+            {
+                var existingPartyGame = this.Data
+                    .PartyGames
+                    .GetById(partyGame.Id);
+                this.Data.PartyGames.Delete(existingPartyGame);
+                this.Data.SaveChanges();
+
+                return RedirectToAction("Index", "ManagePartyGames");
+            }
+
+            return View(partyGame);
+        }
+
     }
 }
