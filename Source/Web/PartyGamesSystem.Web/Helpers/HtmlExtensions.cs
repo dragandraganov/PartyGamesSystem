6327b5a5fcd39f94a3c8f34f0700d5938663ed1a
Dr4g0 <d_d0003@yahoo.com>
2015-01-22 13:46:53 +0200

    Added game description limit in list view with link to Details

---
+++ Source/Web/PartyGamesSystem.Web/Helpers/HtmlExtensions.cs
@@ -0,0 +1,21 @@
+﻿using PartyGamesSystem.Web.ViewModels;
+using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace PartyGamesSystem.Web.Helpers
+{
+    public static class HtmlExtensions
+    {
+        public static IHtmlString ShowDescription(this HtmlHelper html, PartyGameViewModel partyGame)
+        {
+            if (partyGame.Description.Length <= 200)
+            {
+                return new HtmlString(partyGame.Description);
+            }
+            return new HtmlString(string.Format("{0}{1}", partyGame.Description.Substring(0, 200), html.Encode(html.ActionLink("...", "Details", "ManagePartyGames", new { area = string.Empty, id = partyGame.Id }, new { title="Show more"}))));
+        }
+    }
+}
