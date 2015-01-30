9c245ac305cdd9837c12da312348d69cc8bce010
Dr4g0 <d_d0003@yahoo.com>
2015-01-30 22:47:42 +0200

    Added admin in migrations

--- Source/Data/PartyGamesSystem.Data/Migrations/Configuration.cs
+++ Source/Data/PartyGamesSystem.Data/Migrations/Configuration.cs
@@ -1,13 +1,18 @@
 namespace PartyGamesSystem.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using PartyGamesSystem.Common;
     using PartyGamesSystem.Data.Models;
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
     public sealed class Configuration : DbMigrationsConfiguration<PartyGamesSystemDbContext>
     {
+
+        private UserManager<User> userManager;
+
         public Configuration()
         {
             this.AutomaticMigrationsEnabled = true;
@@ -17,6 +22,19 @@ namespace PartyGamesSystem.Data.Migrations
 
         protected override void Seed(PartyGamesSystemDbContext context)
         {
+
+            if (context.Users.Where(u=>u.Email=="bategoyko@bg.bg").Count()==0)
+            {
+                this.userManager = new UserManager<User>(new UserStore<User>(context));
+
+                var user = new User();
+                user.UserName = "bategoyko@bg.bg";
+                user.Email = user.UserName;
+                this.userManager.Create(user, "Dr@g06");
+
+                this.userManager.AddToRole(user.Id, GlobalConstants.AdminRole);
+            }
+            
         }
     }
 }
